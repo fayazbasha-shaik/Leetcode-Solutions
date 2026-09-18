0040755a61dd1e817a84396791c11499fa2aed13
CodeSync <codesync@users.noreply.github.com>
2026-09-18 11:45:20 +0530

    Solved: - (LeetCode) Sheets: Blind75, Grind75, Grind169, NeetCode150, TopInterview150, StriverA2Z, NishantChahar151, ApnaCollege375, FrazBhaiya, AlgoMaster75, SixComp30Days, Striver79, AtharvaPatil150

---
+++ CodeSync/Atharva Patil-s 150 Sheet/-/solution.c
@@ -0,0 +1,14 @@
+/*
+ * Platform: LeetCode
+ * Problem: -
+ * URL: https://leetcode.com/problems/valid-palindrome-ii/description/
+ * Language: C
+ * Difficulty: Easy
+ * Topics: Two Pointers, String, Greedy
+ * Runtime: N/A
+ * Memory: N/A
+ * Synced: 2026-09-18T05:41:33.333Z
+ */
+
+Input: s = "aba"
+Output: true
